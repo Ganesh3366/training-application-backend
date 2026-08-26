b4cb610703175866981fcc420a949d5fda80ba59
Ganesh3366 <ganeshmuthupandian96@gmail.com>
2026-08-27 01:35:30 +0530

    chore: initialize spring boot backend

---
+++ src/main/java/com/ganesh/training_application_backend/TrainingApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh.training_application_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingApplicationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganesh/training_application_backend/TrainingApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganesh.training_application_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
